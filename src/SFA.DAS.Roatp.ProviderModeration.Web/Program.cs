ea2ec48aee10cd519f4359e92da05f5cdb665d85
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-12-01 11:39:15 +0000

    CSP-380 Tribal provider search-wip

--- src/SFA.DAS.Roatp.ProviderModeration.Web/Program.cs
+++ src/SFA.DAS.Roatp.ProviderModeration.Web/Program.cs
@@ -52,9 +52,13 @@ public static class Program
             .UseAuthentication()
             .UseAuthorization();
 
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
 
         app.Run();
     }
