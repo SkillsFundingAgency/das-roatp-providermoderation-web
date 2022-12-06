de09c839e5eafce7705da4581cf09287713d8e9d
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-12-06 18:31:36 +0000

    CSP-380 added validator unit tests

---
+++ src/SFA.DAS.Roatp.ProviderModeration.Web.UnitTests/Validators/ProviderSearchSubmitModelValidatorTests/ValidateUkprnTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentValidation.TestHelper;
+using NUnit.Framework;
+using SFA.DAS.Roatp.ProviderModeration.Web.Models;
+using SFA.DAS.Roatp.ProviderModeration.Web.Validators;
+
+namespace SFA.DAS.Roatp.ProviderModeration.Web.UnitTests.Validators.ProviderSearchSubmitModelValidatorTests
+{
+    [TestFixture]
+    public class ValidateUkprnTests
+    {
+        [TestCase(null)]
+        public void WhenUkprnEmpty_ProducesValidatonError(int? ukprn)
+        {
+            var sut = new ProviderSearchSubmitModelValidator();
+
+            var model = new ProviderSearchSubmitModel()
+            {
+                Ukprn = ukprn,
+            };
+
+            var result = sut.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(c => c.Ukprn)
+                  .WithErrorMessage(ProviderSearchSubmitModelValidator.UkprnEmptyMessage);
+        }
+
+        [TestCase(10000000)]
+        [TestCase(100000000)]
+        public void WhenUkprnOutOfRange_ProducesValidatonError(int? ukprn)
+        {
+            var sut = new ProviderSearchSubmitModelValidator();
+
+            var model = new ProviderSearchSubmitModel()
+            {
+                Ukprn = ukprn,
+            };
+
+            var result = sut.TestValidate(model);
+
+            result.ShouldHaveValidationErrorFor(c => c.Ukprn)
+                  .WithErrorMessage(ProviderSearchSubmitModelValidator.InvalidUkprnErrorMessage);
+        }
+
+        [TestCase(99999998)]
+        [TestCase(12345678)]
+        public void WhenUkprnWithInRange_ProducesValidatonNoError(int? ukprn)
+        {
+            var sut = new ProviderSearchSubmitModelValidator();
+
+            var model = new ProviderSearchSubmitModel()
+            {
+                Ukprn = ukprn,
+            };
+
+            var result = sut.TestValidate(model);
+
+            result.ShouldNotHaveValidationErrorFor(c => c.Ukprn);
+        }
+    }
+}
