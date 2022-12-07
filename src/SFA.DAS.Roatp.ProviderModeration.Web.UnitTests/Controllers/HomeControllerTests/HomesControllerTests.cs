7e8e76da86b045d7ce5f18fdadb83b5b4bd7e558
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-12-07 09:27:42 +0000

    CSP-380 Home controller unit tests updated

--- src/SFA.DAS.Roatp.ProviderModeration.Web.UnitTests/Controllers/HomeControllerTests/HomesControllerTests.cs
+++ src/SFA.DAS.Roatp.ProviderModeration.Web.UnitTests/Controllers/HomeControllerTests/HomesControllerTests.cs
@@ -1,8 +1,12 @@
 ﻿using FluentAssertions;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using NUnit.Framework;
+using SFA.DAS.Roatp.ProviderModeration.Application.Queries.GetProvider;
+using SFA.DAS.Roatp.ProviderModeration.Domain.ApiModels;
 using SFA.DAS.Roatp.ProviderModeration.Web.Configuration;
 using SFA.DAS.Roatp.ProviderModeration.Web.Controllers;
 
@@ -11,15 +15,24 @@ namespace SFA.DAS.Roatp.ProviderModeration.Web.UnitTests.Controllers.HomeControl
     [TestFixture]
     public class HomeControllerTests
     {
+        private HomeController sut;
+        private Mock<IOptions<ApplicationConfiguration>> mockOptions;
+        private ApplicationConfiguration config;
+
+        [SetUp]
+        public void Before_each_test()
+        {
+            mockOptions = new Mock<IOptions<ApplicationConfiguration>>();
+            config = new ApplicationConfiguration() { EsfaAdminServicesBaseUrl = @"https://test.com" };
+            mockOptions.Setup(o => o.Value).Returns(config);
+            sut = new HomeController(mockOptions.Object);
+        }
+
         [Test]
         public void Index_RedirectToAction()
         {
-            var mockOptions = new Mock<IOptions<ApplicationConfiguration>>();
-            var config = new ApplicationConfiguration() { EsfaAdminServicesBaseUrl = @"https://test.com" };
-            mockOptions.Setup(o => o.Value).Returns(config);
-            var sut = new HomeController(mockOptions.Object);
-
             var result = sut.Index() as RedirectToActionResult;
+
             result.Should().NotBeNull();
             result.ActionName.Should().Be("Index");
             result.ControllerName.Should().Be("ProviderSearch");
@@ -28,12 +41,8 @@ namespace SFA.DAS.Roatp.ProviderModeration.Web.UnitTests.Controllers.HomeControl
         [Test]
         public void Dashboard_RedirectToAdminServicesBaseUrl()
         {
-            var mockOptions = new Mock<IOptions<ApplicationConfiguration>>();
-            var config = new ApplicationConfiguration() { EsfaAdminServicesBaseUrl = @"https://test.com" };
-            mockOptions.Setup(o => o.Value).Returns(config);
-            var sut = new HomeController(mockOptions.Object);
-
             var result = sut.Dashboard() as RedirectResult;
+
             result.Should().NotBeNull();
             result.Url.Contains("/dashboard");
         }
