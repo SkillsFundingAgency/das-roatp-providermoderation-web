cd5bc27adea7f287c4d6c516e26cdf26ec4ad0b3
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-12-20 20:42:01 +0000

    CSP-433 added unit tests for review provider description

--- src/SFA.DAS.Roatp.ProviderModeration.Web.UnitTests/Controllers/ProviderDescriptionReviewControllerTests/ProviderDescriptionReviewControllerGetTests.cs
+++ src/SFA.DAS.Roatp.ProviderModeration.Web.UnitTests/Controllers/ProviderDescriptionReviewControllerTests/ProviderDescriptionReviewControllerGetTests.cs
@@ -13,15 +13,16 @@ using SFA.DAS.Roatp.ProviderModeration.Web.Controllers;
 using SFA.DAS.Roatp.ProviderModeration.Web.Infrastructure;
 using SFA.DAS.Roatp.ProviderModeration.Web.Models;
 
-namespace SFA.DAS.Roatp.ProviderModeration.Web.UnitTests.Controllers.ProviderDescriptionAddControllerTests
+namespace SFA.DAS.Roatp.ProviderModeration.Web.UnitTests.Controllers.ProviderDescriptionReviewControllerTests
 {
     [TestFixture]
-    public class ProviderDescriptionAddControllerGetTests
+    public class ProviderDescriptionReviewControllerGetTests
     {
         private Mock<IMediator> _mediatorMock;
-        private ProviderDescriptionAddController _sut;
+        private ProviderDescriptionReviewController _sut;
         private Mock<IUrlHelper> _urlHelperMock;
         string verifyUrl = "http://test";
+        string verifyEditUrl = "http://test-edit";
 
         [SetUp]
         public void Before_Each_Test()
@@ -34,8 +35,16 @@ namespace SFA.DAS.Roatp.ProviderModeration.Web.UnitTests.Controllers.ProviderDes
                .Setup(m => m.RouteUrl(It.Is<UrlRouteContext>(c => c.RouteName.Equals(RouteNames.GetProviderDetails))))
                .Returns(verifyUrl);
 
-            _sut = new ProviderDescriptionAddController(_mediatorMock.Object, Mock.Of<ILogger<ProviderDescriptionAddController>>());
+            _urlHelperMock
+               .Setup(m => m.RouteUrl(It.Is<UrlRouteContext>(c => c.RouteName.Equals(RouteNames.GetReviewProviderDescriptionEdit))))
+               .Returns(verifyEditUrl);
+
+            _sut = new ProviderDescriptionReviewController(_mediatorMock.Object, Mock.Of<ILogger<ProviderDescriptionReviewController>>());
             _sut.Url = _urlHelperMock.Object;
+
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            _sut.TempData = tempData;
         }
 
         [Test, AutoData]
@@ -47,18 +56,26 @@ namespace SFA.DAS.Roatp.ProviderModeration.Web.UnitTests.Controllers.ProviderDes
                 .Setup(m => m.Send(It.Is<GetProviderQuery>(q => q.Ukprn == ukprn), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(providerQueryResult);
 
-            var httpContext = new DefaultHttpContext();
-            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
-            _sut.TempData = tempData;
-
             var result = await _sut.Index(ukprn);
 
             var viewResult = result as ViewResult;
             viewResult.Should().NotBeNull();
-            viewResult?.ViewName.Should().Contain(ProviderDescriptionAddController.ViewPath);
-            var model = viewResult.Model as ProviderDescriptionAddViewModel;
+            viewResult?.ViewName.Should().Contain(ProviderDescriptionReviewController.ViewPath);
+            var model = viewResult.Model as ProviderDescriptionReviewViewModel;
             model.Should().NotBeNull();
             model.CancelLink.Should().Be(verifyUrl);
+            model.EditEntry.Should().Be(verifyEditUrl);
+        }
+
+        [Test, AutoData]
+        public void EditEntry_ValidRequest_ReturnsView(
+           int ukprn)
+        {
+            var result = _sut.EditEntry(ukprn);
+
+            var redirectResult = result as RedirectToRouteResult;
+            redirectResult.Should().NotBeNull();
+            redirectResult.RouteName.Should().Be(RouteNames.GetAddProviderDescription);
         }
     }
 }
---
+++ src/SFA.DAS.Roatp.ProviderModeration.Web.UnitTests/Controllers/ProviderDescriptionReviewControllerTests/ProviderDescriptionReviewControllerPostTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.ProviderModeration.Web.Controllers;
+using SFA.DAS.Roatp.ProviderModeration.Web.Infrastructure;
+using SFA.DAS.Roatp.ProviderModeration.Web.Models;
+
+namespace SFA.DAS.Roatp.ProviderModeration.Web.UnitTests.Controllers.ProviderDescriptionReviewControllerTests
+{
+    [TestFixture]
+    public class ProviderDescriptionReviewControllerPostTests
+    {
+        private Mock<IMediator> _mediatorMock;
+        private ProviderDescriptionReviewController _sut;
+        private Mock<IUrlHelper> _urlHelperMock;
+        string verifyUrl = "http://test";
+        string verifyEditUrl = "http://test-edit";
+        public const int Ukprn = 12345678;
+        public const string LegalName = "TestLegalName";
+        public const string ProviderDescription = "TestProviderDescription";
+
+        [SetUp]
+        public void Before_Each_Test()
+        {
+            _mediatorMock = new Mock<IMediator>();
+
+            _urlHelperMock = new Mock<IUrlHelper>();
+
+            _urlHelperMock
+                .Setup(m => m.RouteUrl(It.Is<UrlRouteContext>(c => c.RouteName.Equals(RouteNames.GetProviderDetails))))
+                .Returns(verifyUrl);
+
+            _urlHelperMock
+               .Setup(m => m.RouteUrl(It.Is<UrlRouteContext>(c => c.RouteName.Equals(RouteNames.GetReviewProviderDescriptionEdit))))
+               .Returns(verifyEditUrl);
+
+            _sut = new ProviderDescriptionReviewController(_mediatorMock.Object, Mock.Of<ILogger<ProviderDescriptionReviewController>>());
+            _sut.Url = _urlHelperMock.Object;
+
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            _sut.TempData = tempData;
+        }
+
+        [Test]
+        public void ReviewProviderDescription_ValidResponseReturnsSameView()
+        {
+            var submitModel = new ProviderDescriptionReviewViewModel
+            {
+                Ukprn = Ukprn,
+                LegalName = LegalName,
+                ProviderDescription = ProviderDescription
+            };
+
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+            tempData["ProviderDescription"] = submitModel.ProviderDescription;
+            _sut.TempData = tempData;
+
+            var result = _sut.ReviewProviderDescription(submitModel);
+
+            var viewResult = result as ViewResult;
+            viewResult.Should().NotBeNull();
+            viewResult.ViewName.Should().Contain(ProviderDescriptionReviewController.ViewPath);
+            var model = viewResult.Model as ProviderDescriptionReviewViewModel;
+            model.Should().NotBeNull();
+            model.Ukprn.Should().Be(submitModel.Ukprn);
+            model.LegalName.Should().Be(submitModel.LegalName);
+            model.ProviderDescription.Should().Be(submitModel.ProviderDescription);
+            model.CancelLink.Should().Be(verifyUrl);
+            model.EditEntry.Should().Be(verifyEditUrl);
+        }
+    }
+}
